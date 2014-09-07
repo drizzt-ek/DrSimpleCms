08059fb6a47ceb2051810b9810113641459d9dd1
Krzysiek <k.nowak@netigen.pl>
2014-09-07 15:29:05 +0200

    Usunięcie noticeów z dispatchera i dodanie komentarzy

--- application/config/core/Dispatcher.php
+++ application/config/core/Dispatcher.php
@@ -14,7 +14,7 @@ class Dispatcher
 
     public function __construct()
     {
-        $this->_url = $_SERVER['REDIRECT_URL'];
+        $this->_url = empty($_SERVER['REDIRECT_URL'])?null:$_SERVER['REDIRECT_URL'];
     }
 
     public function dispatch()
@@ -51,8 +51,22 @@ class Dispatcher
      */
     private function _prepareUrl()
     {
+        //Jeżeli zmienna url jest pusta to nie ma z czego wyciagać nazw kontrolera i akcji.
+        if (empty($this->_url)) {
+            return;
+        }
+        //Rozbijamy adres ze zmiennej $_SERVER po / i otrzymujemy (powinniśmy otrzymać) tablice z akcja i kontrolerem
         $urlArray = explode('/', $this->_url);
-        $this->_controller = strtolower($urlArray[1]) . 'Controller';
-        $this->_action = strtolower($urlArray[2]) . 'Action';
+        //Pierwszy element tablicy to element pusty
+        //Drugi element tablicy powinien być controlerem
+        if (!empty($urlArray[1])) {
+            $this->_controller = strtolower($urlArray[1]) . 'Controller';
+        }
+        //Trzeci element tablicy powinien być akcją
+        if (!empty($urlArray[2])) {
+            $this->_action = strtolower($urlArray[2]) . 'Action';
+        }
+
+        //TODO Może w przyszłości dodamy obsługę parametrów z urla zamiast z geta.
     }
 } 
